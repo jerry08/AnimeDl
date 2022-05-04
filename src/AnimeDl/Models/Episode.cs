34a5596b997744e507d437db3381e20e6ae84db5
Jeremy <74879119+jerry08@users.noreply.github.com>
2022-05-04 15:34:01 -0400

    Added episode description

--- src/AnimeDl/Models/Episode.cs
+++ src/AnimeDl/Models/Episode.cs
@@ -5,11 +5,21 @@ namespace AnimeDl
     public class Episode
     {
         public int Id { get; set; }
+
         public int AnimeId { get; set; }
+
         public string EpisodeName { get; set; }
+
+        public string Description { get; set; }
+
         public float EpisodeNumber { get; set; }
+
         public string EpisodeLink { get; set; }
+
+        public string Image { get; set; }
+
         public DateTime? CreatedAt { get; set; }
+
         public DateTime? UpdatedAt { get; set; }
     }
 }
