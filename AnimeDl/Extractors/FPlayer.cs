8dc13fbe094d470f3966e0d99ef2650d67e3aa69
Jeremy <74879119+jerry08@users.noreply.github.com>
2022-09-18 16:41:43 -0400

    Added extractor

---
+++ AnimeDl/Extractors/FPlayer.cs
@@ -0,0 +1,49 @@
+﻿using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using Newtonsoft.Json.Linq;
+
+namespace AnimeDl.Extractors;
+
+internal class FPlayer : BaseExtractor
+{
+    public FPlayer(NetHttpClient netHttpClient) : base(netHttpClient)
+    {
+    }
+
+    public override async Task<List<Quality>> ExtractQualities(string url)
+    {
+        var apiLink = url.Replace("/v/", "/api/source/");
+
+        var list = new List<Quality>();
+
+        try
+        {
+            var headers = new NameValueCollection()
+            {
+                { "Referer", url }
+            };
+
+            var json = await _netHttpClient.PostAsync(apiLink, headers);
+            if (!string.IsNullOrEmpty(json))
+            {
+                var data = JArray.Parse(JObject.Parse(json)["data"]!.ToString());
+                for (int i = 0; i < data.Count; i++)
+                {
+                    list.Add(new()
+                    {
+                        QualityUrl = data[i]["file"]!.ToString(),
+                        Resolution = data[i]["label"]!.ToString(),
+                        IsM3U8 = data[i]["file"]!.ToString().Contains(".m3u8"),
+                        FileType = data[i]["type"]!.ToString(),
+                    });
+                }
+
+                return list;
+            }
+        }
+        catch { }
+
+        return list;
+    }
+}
