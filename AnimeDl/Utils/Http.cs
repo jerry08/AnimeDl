df08c1f0524cbed1f8e803b2d90803480e52f8ca
jerry08 <jeremyyearwood99@gmail.com>
2022-10-11 16:39:27 -0400

    Made class public

--- AnimeDl/Utils/Http.cs
+++ AnimeDl/Utils/Http.cs
@@ -4,7 +4,7 @@ using System.Net.Security;
 
 namespace AnimeDl.Utils;
 
-internal static class Http
+public static class Http
 {
     private static readonly Lazy<HttpClient> HttpClientLazy = new(() =>
     {
