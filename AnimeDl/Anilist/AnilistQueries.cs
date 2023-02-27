bb6ee0041c254caa1dcd2e00c4501a856c2ae447
jerrry08 <jeremyyearwood99@gmail.com>
2023-02-27 19:59:37 -0400

    Cleanup

--- AnimeDl/Anilist/AnilistQueries.cs
+++ AnimeDl/Anilist/AnilistQueries.cs
@@ -4,7 +4,7 @@ public class AnilistQueries
 {
     public static string Trending(int page, int perPage, string type)
     {
-        return @"query ($page: Int = ${page}, $id: Int, $type: MediaType = ${type}, $isAdult: Boolean = false, $size: Int = ${perPage}, $sort: [MediaSort] = [TRENDING_DESC, POPULARITY_DESC]) { Page(page: $page, perPage: $size) { pageInfo { total perPage currentPage lastPage hasNextPage } media(id: $id, type: $type, isAdult: $isAdult, sort: $sort) { id idMal status(version: 2) title { userPreferred romaji english native } genres trailer { id site thumbnail } description format bannerImage coverImage{ extraLarge large medium color } episodes meanScore duration season seasonYear averageScore nextAiringEpisode { airingAt timeUntilAiring episode }  } } }"
+        return "query ($page: Int = ${page}, $id: Int, $type: MediaType = ${type}, $isAdult: Boolean = false, $size: Int = ${perPage}, $sort: [MediaSort] = [TRENDING_DESC, POPULARITY_DESC]) { Page(page: $page, perPage: $size) { pageInfo { total perPage currentPage lastPage hasNextPage } media(id: $id, type: $type, isAdult: $isAdult, sort: $sort) { id idMal status(version: 2) title { userPreferred romaji english native } genres trailer { id site thumbnail } description format bannerImage coverImage{ extraLarge large medium color } episodes meanScore duration season seasonYear averageScore nextAiringEpisode { airingAt timeUntilAiring episode }  } } }"
             .Replace("${page}", $"{page}")
             .Replace("${perPage}", $"{perPage}")
             .Replace("${type}", $"{type}");
